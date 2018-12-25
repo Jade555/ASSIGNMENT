39306bcb193119e63788f2eec075cacbf24fc44a
Ronnakron <ronnakron.r@outlook.com>
2018-12-25 12:11:06 +0700

    [Jade] create cache data in vue.js and nodejs express server

--- test-ws/server.js
+++ test-ws/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+var cache = require('memory-cache');
 app.get('/q1/:index', (req, res) => {
+
 
 
     let index = req.params.index;
@@ -41,35 +42,83 @@ app.listen(port, () => console.log(`app listening on port ${port}!`))
 
 function seriresNumber1(startNumber, index) {
 
-    let number = Number(startNumber);
-    let results = []
-    for (var i = 1; i <= index; i++) {
-        number = number + (2 * (i - 1));
-        results.push(number);
+    /* 
+        1. Check Cache data from memory cache
+        2. return data
+    */
+    let cacheData = []
+    let localData = cache.get("ansQ1");
+    if (localData != null) {
+        cacheData = localData;
     }
+    var findKey = cacheData.filter(f => f.key == index);
+    let results = []
+    if (findKey.length == 0) {
+        let number = Number(startNumber);
 
+        for (var i = 1; i <= index; i++) {
+            number = number + (2 * (i - 1));
+            results.push(number);
+        }
+        let tmpCache = {
+            key: index,
+            value: results
+        }
+        cacheData.push(tmpCache);
+        cache.put("ansQ1", cacheData);
+    } else {
+        results = findKey[0].value;
+    }
     return results;
 }
 
 function seriesNumber2() {
-    let result = 99 - 20 - 24
+    let localData = cache.get("ansQ2");
+    let result = 0;
+    if (!localData) {
+        result = 99 - 20 - 24
+        cache.put("ansQ2", result);
+    } else {
+        result = localData;
+    }
+
     return result;
 }
 
 function seriesNumber3(index) {
-
+    /* 
+        1. Check Cache data from memory cache
+        2. return data
+    */
+    let cacheData = []
+    let localData = cache.get("ansQ3");
+    if (localData != null) {
+        cacheData = localData;
+    }
+    var findKey = cacheData.filter(f => f.key == index);
     let results = [{
         index: 1,
         value: 5
     }]
-    for (var i = 1; i < index; i++) {
-        var resultTmp = results.filter(f => f.index == i)[0];
-        var tmp = (i + 1) + "" + resultTmp.value;
-        temp = {
-            index: i + 1,
-            value: tmp
+    if (findKey.length == 0) {
+        for (var i = 1; i < index; i++) {
+            var resultTmp = results.filter(f => f.index == i)[0];
+            var tmp = (i + 1) + "" + resultTmp.value;
+            temp = {
+                index: i + 1,
+                value: tmp
+            }
+            results.push(temp);
         }
-        results.push(temp);
+        let tmpCache = {
+            key: index,
+            value: results
+        }
+        cacheData.push(tmpCache);
+        cache.put("ansQ3", cacheData);
+    } else {
+        results = findKey[0].value;
     }
+
     return results;
 }
--- test/src/components/test.vue
+++ test/src/components/test.vue
@@ -91,80 +91,155 @@ export default {
   },
   methods: {
     findQ1: function(event) {
+      /* 
+        1. Check input field
+        2. Check cache data (LocalStorage)
+        3. Call WebService (Nodejs Server)
+        4. try catch webservice 
+        5. Return data to view
+      */
       if (this.inputQ1 != "") {
-        const baseURI = "http://localhost:3000/q1/" + this.inputQ1;
-        this.$http
-          .get(baseURI)
-          .then(result => {
-            var data = result.data;
-            if (data) {
-              this.ansQ1 = data.results;
-            }
-          })
-          .catch(e => {
-            // when cannot call web service
-            let number = Number(3);
-            let results = [];
-            for (var i = 1; i <= this.inputQ1; i++) {
-              number = number + 2 * (i - 1);
-              results.push(number);
-            }
-            this.ansQ1 = results;
-          });
+        let dataList = [];
+        let localData = localStorage.getItem("ansQ1");
+        if (localData) {
+          dataList = JSON.parse(localData);
+        }
+        let findKey = dataList.filter(f => f.key == this.inputQ1);
+        if (findKey.length == 0) {
+          const baseURI = "http://localhost:3000/q1/" + this.inputQ1;
+          this.$http
+            .get(baseURI)
+            .then(result => {
+              var data = result.data;
+              if (data) {
+                this.ansQ1 = data.results;
+                let tmp = {
+                  key: this.inputQ1,
+                  value: this.ansQ1
+                };
+                dataList.push(tmp);
+                localStorage.setItem("ansQ1", JSON.stringify(dataList));
+              }
+            })
+            .catch(e => {
+              // when cannot call web service
+              let number = Number(3);
+              let results = [];
+              for (var i = 1; i <= this.inputQ1; i++) {
+                number = number + 2 * (i - 1);
+                results.push(number);
+              }
+              this.ansQ1 = results;
+              let tmp = {
+                key: this.inputQ1,
+                value: this.ansQ1
+              };
+              dataList.push(tmp);
+              localStorage.setItem("ansQ1", JSON.stringify(dataList));
+            });
+        } else {
+          // return data from localstorage
+          this.ansQ1 = findKey[0].value;
+        }
       }
     },
     findQ2: function(event) {
-      const baseURI = "http://localhost:3000/q2";
-      this.$http
-        .get(baseURI)
-        .then(result => {
-          var data = result.data;
-          if (data) {
-            this.ansQ2 = data.results;
-          }
-        })
-        .catch(e => {
-          /* When call not call web service  */
-          let result = 99 - 20 - 24;
-          this.ansQ2 = result;
-        });
-    },
-    findQ3: function(event) {
-      if (this.inputQ3 != "") {
-        this.ansQ3 = [];
-        const baseURI = "http://localhost:3000/q3/" + this.inputQ3;
+      /* 
+        1. Check cache data (LocalStorage)
+        2. Call WebService (Nodejs Server)
+        3. try catch webservice 
+        4. Return data to view
+      */
+      let localData = localStorage.getItem("ansQ2");
+      if (!localData) {
+        const baseURI = "http://localhost:3000/q2";
         this.$http
           .get(baseURI)
           .then(result => {
             var data = result.data;
             if (data) {
-              let tmp = data.results;
-              tmp.forEach(e => {
-                this.ansQ3.push(e.value);
-              });
+              this.ansQ2 = data.results;
+              localStorage.setItem("ansQ2", this.ansQ2);
             }
           })
           .catch(e => {
             /* When call not call web service  */
-            let results = [
-              {
-                index: 1,
-                value: 5
-              }
-            ];
-            for (var i = 1; i < this.inputQ3; i++) {
-              var resultTmp = results.filter(f => f.index == i)[0];
-              var tmp = i + 1 + "" + resultTmp.value;
-              let temp = {
-                index: i + 1,
-                value: tmp
-              };
-              results.push(temp);
-            }
-            results.forEach(e => {
-              this.ansQ3.push(e.value);
-            });
+            let result = 99 - 20 - 24;
+            this.ansQ2 = result;
+            localStorage.setItem("ansQ2", this.ansQ2);
           });
+      } else {
+        this.ansQ2 = localData;
+      }
+    },
+    findQ3: function(event) {
+      /* 
+        1. Check input field
+        2. Check cache data (LocalStorage)
+        3. Call WebService (Nodejs Server)
+        4. try catch webservice 
+        5. Return data to view
+      */
+
+      if (this.inputQ3 != "") {
+        let dataList = [];
+        let localData = localStorage.getItem("ansQ3");
+        if (localData) {
+          dataList = JSON.parse(localData);
+        }
+        let findKey = dataList.filter(f => f.key == this.inputQ3);
+
+        if (findKey.length == 0) {
+          this.ansQ3 = [];
+          const baseURI = "http://localhost:3000/q3/" + this.inputQ3;
+          this.$http
+            .get(baseURI)
+            .then(result => {
+              var data = result.data;
+              if (data) {
+                let tmp = data.results;
+                tmp.forEach(e => {
+                  this.ansQ3.push(e.value);
+                });
+
+                let tmpLocal = {
+                  key: this.inputQ3,
+                  value: this.ansQ3
+                };
+                dataList.push(tmpLocal);
+                localStorage.setItem("ansQ3", JSON.stringify(dataList));
+              }
+            })
+            .catch(e => {
+              /* When call not call web service  */
+              let results = [
+                {
+                  index: 1,
+                  value: 5
+                }
+              ];
+              for (var i = 1; i < this.inputQ3; i++) {
+                var resultTmp = results.filter(f => f.index == i)[0];
+                var tmp = i + 1 + "" + resultTmp.value;
+                let temp = {
+                  index: i + 1,
+                  value: tmp
+                };
+                results.push(temp);
+              }
+              results.forEach(e => {
+                this.ansQ3.push(e.value);
+              });
+              let tmpLocal = {
+                key: this.inputQ3,
+                value: this.ansQ3
+              };
+              dataList.push(tmpLocal);
+              localStorage.setItem("ansQ3", JSON.stringify(dataList));
+            });
+        } else {
+          this.ansQ3 = findKey[0].value;
+        }
       }
     }
   }
